56c6ef3cded53857227af06ea01ed315a96dff64
LeoTianJob <leo.tian@ap.jll.com>
2020-03-01 01:02:32 +0800

    add temp code on the server side.

--- api/api/routing.py
+++ api/api/routing.py
@@ -1,5 +1,12 @@
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+import orderbook.routing
 
 application = ProtocolTypeRouter({
-
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            orderbook.routing.websocket_urlpatterns
+        )
+    ),
 })
